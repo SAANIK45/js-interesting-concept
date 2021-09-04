178ba9b0cf58ce7296a989e2b57207ce99b86244
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-04 20:34:53 +0600

    update

---
+++ set-timeout.js
@@ -0,0 +1,7 @@
+console.log('this is first');
+console.log('this is second');
+//setTimeout is a function here we can make time schedule
+setTimeout(function(){
+    console.log('from set timeout');
+},5000)
+//step by step execution called synchronous
